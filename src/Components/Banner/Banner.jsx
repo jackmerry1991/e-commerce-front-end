b13fc1416f35fa9022eb4556db7465792d6f2017
Jack - <jackmerry123@hotmail.com>
2021-11-01 17:55:52 +0000

    Banner drop down menu implemented

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import './Banner.css'
-import logo from '../../Images/e-commerce-logo.png'
+import menuIcon from '../../Images/menu-icon.svg'
+import closeIcon from '../../Images/close-menu.svg'
 import searchByProduct from '../../UtilityFunctions/api'
+
+
 
 const Banner = () => {
     const [searchTerm, setSearchTerm] = useState('')
+    const [dropDownIsOpen, setDropDownIsOpen] = useState(false);
+    const [dropDownMenuClass, setDropDownMenuClass] = useState('mobile-drop-down-closed');
+    const [overlayClass, setOverlayClass] = useState('menu-overlay');
 
     const handleChange = (event) => {
         setSearchTerm(event.target.value)
@@ -13,17 +19,34 @@ const Banner = () => {
     const handleSubmit = () => {
         searchByProduct(searchTerm)
     }
+    
+    const toggleDropDown = () => {
+        setDropDownIsOpen(!dropDownIsOpen);
+        console.log('open');
+        console.log(dropDownIsOpen)
+        if(!dropDownIsOpen){
+            setDropDownMenuClass('mobile-drop-down-open')
+            setOverlayClass('menu-overlay-active');
+        }else{
+            setDropDownMenuClass('mobile-drop-down-closed');
+            setOverlayClass('menu-overlay');
+        }
+    }
+
     return (
         <div className="banner">
+            <div className={overlayClass} role="presentation" onClick={toggleDropDown}/>
             <div className="banner-left">
-                <img className="banner-logo" src={logo} alt="site-logo" />
+                <a href="/Home" className="banner-logo-link">
+                <div className="banner-logo-outer">
+                    <h1 className="banner-logo" >e-Store</h1>
+                </div>
+                </a>
             </div>
-            <button className="hamburger-menu-button" type="button">
-                ::
-            </button>
             <li className="banner-links">
                 <ul>Home</ul>
                 <ul>Categories</ul>
+                <ul>Contact Us</ul>
             </li>
             <div className="banner-rightSide">
                 <div className="banner-searchBar">
@@ -33,7 +56,7 @@ const Banner = () => {
                         value={searchTerm}
                         onChange={handleChange}
                     />
-                    <button type="submit" onClick={handleSubmit}>
+                    <button className="banner-searchBar-button" type="submit" onClick={handleSubmit}>
                         Search
                     </button>
                 </div>
@@ -41,6 +64,19 @@ const Banner = () => {
                     <li>Sign in</li>
                     <li>Cart</li>
                 </ul>
+                <div className="menu-container">{
+                    dropDownIsOpen ? <img className="mobile-burger-menu" role="presentation"src={closeIcon} onClick={toggleDropDown} alt="menu"/> : 
+                    <img className="mobile-burger-menu" role="presentation"src={menuIcon} onClick={toggleDropDown} alt="menu"/>
+                }
+                    <div className={dropDownMenuClass} id="menu-open">
+                        <ul>
+                            <a href="/Home">Home</a>
+                            <a href="/ContactUs">Contact Us</a>
+                            <a href="/Cart">Cart</a>
+                            <a href="Sign in">Sign In</a>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
